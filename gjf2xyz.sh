8ab31e26059dc05825f340e832a31ea6d670ae42
RTChou67 <3023207450@tju.edu.cn>
2025-10-24 20:53:15 +0800

    update

---
+++ gjf2xyz.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+icc=0
+nfile=$(ls *.gjf | wc -l)
+for inf in *.gjf; do
+    ((icc++))
+    echo Converting ${inf} to ${inf//gjf/xyz} ... \($icc of $nfile\)
+    Multiwfn ${inf} <<EOF >/dev/null
+100
+2
+2
+${inf//gjf/xyz}
+0
+q
+EOF
+done
